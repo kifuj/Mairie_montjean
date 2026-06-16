eedb2fd20df33971b4f9c4cf53c3b9423efba7b5
Matteo <lamarematteo53@gmail.com>
2026-06-16 10:19:24 +0200

    feat(includes) : creation de function.php et mise en place des horaires

---
+++ includes/function.php
@@ -0,0 +1,12 @@
+<?php
+function getHorairesMairie() {
+    return [
+        'Lundi'    => '9h-12h / 13h45-17h30',
+        'Mardi'    => '9h-12h',
+        'Mercredi' => '9h-12h',
+        'Jeudi'    => '9h-12h',
+        'Vendredi' => '9h-12h / 13h45-17h30',
+        'Samedi'   => 'Fermé',
+        'Dimanche' => 'Fermé',
+    ];
+}
