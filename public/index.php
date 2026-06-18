0531e94445b656f282ee92c2718fab5a87848066
Matteo <lamarematteo53@gmail.com>
2026-06-18 09:32:14 +0200

    feat(index) : remise en place de l'include fonction pour eviter le crash et ajout des widgets panneau pocket

--- public/index.php
+++ public/index.php
@@ -1,11 +1,12 @@
 <?php
 define('APP_RUNNING', true);
 require_once __DIR__ . '/../config/config.php';
+require_once __DIR__ . '/../includes/function.php';
 
 $pageTitle = "Accueil";
 $pageDescription = "Site officiel de la Mairie de Montjean (53320) : actualités, démarches administratives, vie locale et informations pratiques.";
 require_once __DIR__ . '/../includes/header.php';
-include __DIR__ . '/../includes/function.php';
+
 ?>
 
 <!--Banniere-->
@@ -28,7 +29,7 @@ include __DIR__ . '/../includes/function.php';
         m quaerat laudantium eius, veniam a nisi. Sint vero quam eercitationem, dicta commodi porro
         saepe.
     </div>
-    <a href="la-commune/presentation.php" class="btn">en savoir plus</a>
+    <a href="/la-commune/presentation.php" class="btn">en savoir plus</a>
 </section>
 
 <!--Horaire-->
@@ -54,12 +55,12 @@ include __DIR__ . '/../includes/function.php';
     <h2>Actualités</h2>
 
     <div id="panneau-pocket-feed">
-
-        <article>
-            <h3>Titre actualité</h3>
-            <p>Résumé...</p>
-            <a href="#">Lire la suite</a>
-        </article>
+            <iframe
+                src="https://app.panneaupocket.com/embeded/1295380060?mode=widgetConfig&autoNavigation=0&widgetConfigId=1f1697e4-c2b7-6862-afd1-318931428377"
+                style="height:518px;width:1280px; max-height:100%; width: 100%; border:none"></iframe>
+            <iframe
+                src="https://app.panneaupocket.com/embeded/1295380060?mode=widgetConfig&autoNavigation=0&widgetConfigId=1f1697e0-4c68-6c1e-a9f1-4bbb187e512f"
+                style="height:518px;width:1280px; max-height:100%; width: 100%; border:none"></iframe>
 
     </div>
 
